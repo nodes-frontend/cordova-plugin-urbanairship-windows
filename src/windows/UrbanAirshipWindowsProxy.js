80a003ae8cc82bebfdbf0410ffd830e2142bc44a
Johnny Sørensen <joso@nodes.dk>
2017-08-15 18:38:55 +0200

    added debug event + added new working Push Component

--- src/windows/UrbanAirshipWindowsProxy.js
+++ src/windows/UrbanAirshipWindowsProxy.js
@@ -2,9 +2,9 @@ var cordova = require('cordova'),
 	UrbanAirshipWindows = require('./UrbanAirshipWindows');
 
 function listenAndDispatch() {
-	if(RuntimeComponentTest.Class1) {
+	if(PushComponent.PushHandler) {
 		try {
-			RuntimeComponentTest.Class1.addEventListener('registrationstatechanged', function(e) {
+			PushComponent.PushHandler.addEventListener('registrationstatechanged', function(e) {
 				console.info('UrbanAirshipWindows will dispatch Event: ', e);
 				var event = new CustomEvent('urbanairship.registration', { detail: e });
 				document.dispatchEvent(event);
@@ -12,9 +12,19 @@ function listenAndDispatch() {
 		} catch(exception) {
 			console.error('UrbanAirshipWindows could not add Event Listener for registrationstatechanged', exception);
 		}
+
+		try{
+			PushComponent.PushHandler.addEventListener('debugevent', function(e) {
+				console.info('UrbanAirshipWindows will dispatch Event: ', e);
+				var event = new CustomEvent('urbanairship.debugevent',  { detail: e });
+				document.dispatchEvent(event);
+			});
+		} catch(exception) {
+			console.error('UrbanAirshipWindows could not add Event Listener for pushstatechanged', exception);
+		}
 		
 		try{
-			RuntimeComponentTest.Class1.addEventListener('pushstatechanged', function(e) {
+			PushComponent.PushHandler.addEventListener('pushstatechanged', function(e) {
 				console.info('UrbanAirshipWindows will dispatch Event: ', e);
 				var event = new CustomEvent('urbanairship.push',  { detail: e });
 				document.dispatchEvent(event);
@@ -24,7 +34,7 @@ function listenAndDispatch() {
 		}
 		
 		try{
-			RuntimeComponentTest.Class1.addEventListener('pushparseerror', function(e) {
+			PushComponent.PushHandler.addEventListener('pushparseerror', function(e) {
 				console.info('UrbanAirshipWindows will dispatch Event: ', e);
 				var event = new CustomEvent('urbanairship.pusherror', { detail: e });
 				document.dispatchEvent(event);
@@ -41,12 +51,12 @@ module.exports = {
 	init: function(success, failure, uaConfig) {
 		listenAndDispatch();
 		
-		var res = RuntimeComponentTest.Class1.setUAConfig(uaConfig['key'], uaConfig['secret'], uaConfig['production']);
+		var res = PushComponent.PushHandler.setUAConfig(uaConfig['key'], uaConfig['secret'], uaConfig['production']);
 		
 		if (res.indexOf('Error') > -1) {
 			failure(res);
 		} else {
-			var takeOffRes = RuntimeComponentTest.Class1.initUA();
+			var takeOffRes = PushComponent.PushHandler.initUA();
 			if (takeOffRes.indexOf('Error') > -1) failure(takeOffRes);
 			else success(res + '\n' + takeOffRes);
 		}
@@ -54,7 +64,7 @@ module.exports = {
 	
 	setUserNotificationsEnabled: function(success, failure, enabled) {
 		
-		var res = RuntimeComponentTest.Class1.setUserNotificationsEnabled(enabled);
+		var res = PushComponent.PushHandler.setUserNotificationsEnabled(enabled);
 		
 		if (res.indexOf('Error') > -1) {
 			failure(res);
@@ -65,7 +75,7 @@ module.exports = {
 	
 	setAlias: function(success, failure, aliasString) {
 		
-		var res = RuntimeComponentTest.Class1.setApid(aliasString);
+		var res = PushComponent.PushHandler.setApid(aliasString);
 		
 		if (res.indexOf('Error') > -1) {
 			failure(res);
@@ -75,7 +85,7 @@ module.exports = {
 	},
 	
 	getAlias: function(success, failure) {
-		var res = RuntimeComponentTest.Class1.getAlias();
+		var res = PushComponent.PushHandler.getAlias();
 		
 		if (res.indexOf('Error') > -1) {
 			failure(res);
